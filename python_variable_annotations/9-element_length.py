1925915c85eee92adfc24886d4f47e4f627111c5
Cecilia-Banda <c.banda@alustudent.com>
2024-01-20 01:48:42 +0200

    iteration correction

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,14 +5,6 @@
 from typing import Iterable, Sequence, List, Union, Tuple
 
 
-def element_length(lst: Iterable[Sequence])\
-        -> List[Tuple[Sequence, int]]:
-    """
-        Args:
-            lst: Sequence of list
-
-        Return:
-            List of tuple of sequence of integers
-    """
-
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ Element length """
     return [(i, len(i)) for i in lst]
