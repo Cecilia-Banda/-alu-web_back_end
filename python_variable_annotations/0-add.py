4c21e1bd639f24d58fa7c303da1fd24dd40ef584
Cecilia_Banda <105134294+Cecilia-Banda@users.noreply.github.com>
2024-01-22 23:01:34 +0200

    Update 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 """
-    Module for python variable annotations
+Module for python variable annotations
 """
-
-
 
 def add(a: float, b: float) -> float:
     """
-        A sum of two different floats
+    A sum of two different floats
+    
+    Parameters:
+    a (float): The first float.
+    b (float): The second float.
+    
+    Returns:
+    float: The sum of the two floats.
     """
     return a + b
